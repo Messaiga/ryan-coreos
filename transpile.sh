ac26dcba821c2a1786f4f0f5fba093fc3ad25de3
ryan <ryanozzy147@gmail.com>
2024-05-14 16:01:44 -0400

    Fixing transpile error

--- transpile.sh
+++ transpile.sh
@@ -1,3 +1,3 @@
 podman run --interactive --rm --security-opt label=disable \
        --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
-       --pretty --strict ucore-autorebase > ucore-autorebase.ign
+       --pretty --strict ucore-autorebase.butane > ucore-autorebase.ign
